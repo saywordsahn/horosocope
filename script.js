8f39f5d013b16ac2e56fae5f2ff26b2328470c90
TCC - Admin <saywordsahn@gmail.com>
2022-01-08 18:32:11 +0900

    Added images for each sign

--- script.js
+++ script.js
@@ -9,6 +9,72 @@ class Sign {
 
 }
 
+function createSignCard(sign) {
+
+    // create card
+    let card = document.createElement('div');
+    card.classList.add('card')
+
+    // create img
+    let img = document.createElement('img');
+    img.classList.add('card-img-top');
+    img.classList.add('sign-img');
+    img.src = 'img/' + sign.name + '.png';
+    img.alt = sign.name;
+
+    // create body
+    let body = document.createElement('div');
+    body.classList.add("card-body");
+
+    // create title
+    let title = document.createElement('h5');
+    title.classList.add('card-title');
+    title.innerText = sign.name;
+
+    // create button
+    let button = document.createElement('input');
+    button.value = 'Select';
+    button.type = 'button';
+    button.classList.add('btn');
+    button.classList.add('btn-primary');
+    button.addEventListener('click', () => {
+
+        let child = detailedInfo.lastElementChild;
+        while (child) {
+            detailedInfo.removeChild(child);
+            child = detailedInfo.lastElementChild;
+        }
+
+        container.hidden = true;
+        detailedInfo.hidden = false;
+
+        const backButton = document.createElement('input');
+        backButton.value = 'Return';
+        backButton.type = 'button';
+        backButton.classList.add('btn');
+        backButton.classList.add('btn-primary');
+        backButton.addEventListener('click', () => {
+            container.hidden = false;
+            detailedInfo.hidden = true;
+        });
+
+        const img = document.createElement('img');
+        img.classList.add('selected-sign-img');
+        img.src = 'img/' + sign.name + '.png';
+        img.alt = sign.name;
+
+        detailedInfo.appendChild(backButton);
+        detailedInfo.appendChild(img);
+    });
+
+    body.appendChild(title);
+    body.appendChild(button);
+    card.appendChild(img);
+    card.appendChild(body);
+
+    return card;
+}
+
 const signs = [];
 
 signs.push(new Sign('Virgo', 'Brown', 'Mercury'));
@@ -24,29 +90,12 @@ signs.push(new Sign('Aquarius', 'Blue', 'Saturn'));
 signs.push(new Sign('Gemini', 'Yellow', 'Mercury'));
 signs.push(new Sign('Libra', 'Pink', 'Venus'));
 
-// const signs = [aries, sagittarius, leo, pisces, scorpio,
-//     cancer, virgo, capricorn, taurus, aquarius, gemini, libra];
-
-
-console.log(signs);
-
-
-const submitBtn = document.getElementById('submitBtn');
-const signSelect = document.getElementById('signSelect');
-const container = document.getElementById('container');
+const container = document.getElementById('cardContainer');
+const detailedInfo = document.getElementById('detailedInfo');
 
 for (const sign of signs) {
-    let img = document.creat
 
+    const card = createSignCard(sign);
 
+    container.appendChild(card);
 }
-
-
-function btnclicked() {
-    const selectedSign = signSelect.selectedOptions[0].text
-
-    alert('ew  ' + selectedSign)
-}
-
-submitBtn.addEventListener('click', btnclicked);
-
